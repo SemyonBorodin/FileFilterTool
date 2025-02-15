ce929c49c879afd246c80d9b419d05ef6496b301
Semyon <s.borodin@g.nsu.ru>
2025-02-15 17:32:43 +0700

    Exception catching added

--- src/main/java/com/semyon/filtertool/io/FileReaderService.java
+++ src/main/java/com/semyon/filtertool/io/FileReaderService.java
@@ -1,31 +1,51 @@
 package com.semyon.filtertool.io;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileReaderService {
-//    private final String outputPath;
-//    private final String outputPrefix;
-//    private final Boolean AddingMode;
-    public static void readFile(String[] inputFileNames) throws IOException{
-        List<String> lines = new ArrayList<>();
+
+    public static List<String> readFile(String[] inputFileNames){
+        List<String> linesFromFiles = new ArrayList<>();
         List<BufferedReader> bufferedReaders = new ArrayList<>();
         try {
-            for(String inputFileName: inputFileNames){
-                try{
+            for (String inputFileName : inputFileNames) {
+                try {
                     BufferedReader bufferedReader;
-                    bufferedReader =  Files.newBufferedReader(Path.of(inputFileName));
+                    bufferedReader = Files.newBufferedReader(Path.of(inputFileName));
                     bufferedReaders.add(bufferedReader);
-                } catch (IOException err){
+                } catch (IOException err) {
                     System.out.println("Opening file problem:" + err.getMessage());
                 }
+                // TO DO: other errs shoud be handled
             }
-            //file reading: to do
-        } finally {
-            //close files: to do
+        boolean hasReadAnyLine = true;
+
+        while (hasReadAnyLine) {
+            hasReadAnyLine = false;
+            for (BufferedReader bufferedReader : bufferedReaders) {
+                try {
+                    String curLine = bufferedReader.readLine();
+                    if (curLine != null) {
+                        linesFromFiles.add(curLine);
+                        hasReadAnyLine = true;
+                    }
+                } catch (IOException err) {
+                    System.err.println("Read line from file error:" + err.getMessage());
+                }
+            }
         }
+    } finally {
+        for (BufferedReader bufferedReader : bufferedReaders) {
+            try {
+                bufferedReader.close();
+            } catch (IOException err) {
+                System.err.println("Closing file error: " + err.getMessage());
+            }
+        }
+    }
+    return linesFromFiles;
     }
 }
