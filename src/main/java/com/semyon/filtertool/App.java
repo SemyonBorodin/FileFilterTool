64c19b85533e82acbd21b5e26c12a6543b9439c5
Semyon <s.borodin@g.nsu.ru>
2025-02-17 02:17:11 +0700

    just a commit, testing input files processing

--- src/main/java/com/semyon/filtertool/App.java
+++ src/main/java/com/semyon/filtertool/App.java
@@ -13,14 +13,15 @@ public class App
         OptionParser options = new OptionParser(args);
         String[] inputFilesNames = options.getInputFilesNames();
         String outputPath = options.getOutputPath();
-        System.out.println( "Hello World!" + args.length + outputPath
-                + Arrays.toString(inputFilesNames) +"3" + (inputFilesNames[1] instanceof String) + "4");
-        System.out.println("текущая директория " + System.getProperty("user.dir"));
-        DataTypeSplitter splitter = new DataTypeSplitter();
-        splitter.splitByType(List.of("__", "10E-1", "____", "bottom_Text", "__"));
-
-        System.out.println("Integers size (0 => needn't create or adding to file): "
-                + splitter.getIntegers().size());
+        System.out.println(Arrays.toString(inputFilesNames));
+//        System.out.println( "Hello World!" + args.length + outputPath
+//                + Arrays.toString(inputFilesNames) +"3" + (inputFilesNames[1] instanceof String) + "4");
+//        System.out.println("текущая директория " + System.getProperty("user.dir"));
+//        DataTypeSplitter splitter = new DataTypeSplitter();
+//        splitter.splitByType(List.of("__", "10E-1", "____", "bottom_Text", "__"));
+//
+//        System.out.println("Integers size (0 => needn't create or adding to file): "
+//                + splitter.getIntegers().size());
     }
 
 }
