4671cbb94a1d301cc0f787591aaf5b1f3ad31e0a
Semyon <s.borodin@g.nsu.ru>
2025-02-17 02:24:17 +0700

    Prefix validator and sketch of path validator added

--- src/main/java/com/semyon/filtertool/optionsParser/OptionValidator.java
+++ src/main/java/com/semyon/filtertool/optionsParser/OptionValidator.java
@@ -1,18 +1,39 @@
 package com.semyon.filtertool.optionsParser;
 
 public class OptionValidator {
+    // & ; | * ? ' " ` [ ] ( ) $ < > { } ^ # \ / % ! cannot be in file name in linux
+    private static final String bannedSymbols = "[&;\\\\|\\\\*\\\\?\\'\\\"`\\\\[\\\\]\\\\(\\\\)\\\\$<>\\\\{\\\\}\\\\^#\\\\\\\\/%!]";
 
     public static boolean isValidPathFormat(String path){
-        if(path.startsWith("/")){
-            return true;
+        // if path invalid or empty we use default path
+        String[] pathParts = path.split("/");
+        for (String part: pathParts){
+            // ? case when starts with something/lala
+            // need to check  part.isEmpty()) => continue or smt like that?
+            // case .. ?
+            // if we have // in path .split will create "" in path
+            // so if we consider nonempty path I think pathParts cannot contain ""
+            if (part.matches(".*" + bannedSymbols + ".*" ) == true){
+                return false; // path is invalid because contains banned symbol
+            }
+            else {return true;}
         }
-        else {return false;}
+        return false;
     }
 
-    public static boolean isValidPrefixFormat(String path){
-        // & ; | * ? ' " ` [ ] ( ) $ < > { } ^ # \ / % ! cannot be in file name in linux
-        System.out.println('a');
-        return true;
+    public static boolean isValidPrefixFormat(String prefix){
+        if (prefix == null){
+            return false;
+        }
+        if (prefix.isBlank()){
+            return true;
+        }
+        if (prefix.matches(".*" + bannedSymbols + ".*" ) == false){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
