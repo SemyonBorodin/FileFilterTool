bd2d18fde2e11f5980217b93b92094e8534397d8
Semyon <s.borodin@g.nsu.ru>
2025-02-17 02:22:21 +0700

    arg String prefix added to writeToFilde

--- src/main/java/com/semyon/filtertool/io/FileWriterService.java
+++ src/main/java/com/semyon/filtertool/io/FileWriterService.java
@@ -1,9 +1,12 @@
 package com.semyon.filtertool.io;
-
+import java.io.File;
 import java.util.List;
 
 public class FileWriterService {
-    public static void writeToFile(String path, List<String> data,Boolean isAppendMode){
+    public static void writeToFile(String path,
+                                   List<String> data,
+                                   Boolean isAppendMode,
+                                   String prefix){
 
     }
 }
