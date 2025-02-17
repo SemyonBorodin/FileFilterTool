9fc4e932a92e45e91e369b5e9b215d4de690189e
Semyon <s.borodin@g.nsu.ru>
2025-02-17 19:05:10 +0700

    FileWriter servece added

--- src/main/java/com/semyon/filtertool/io/FileWriterService.java
+++ src/main/java/com/semyon/filtertool/io/FileWriterService.java
@@ -1,12 +1,30 @@
 package com.semyon.filtertool.io;
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class FileWriterService {
-    public static void writeToFile(String path,
-                                   List<String> data,
-                                   Boolean isAppendMode,
-                                   String prefix){
 
+    // private final static String DEFAULT_PATH = System.getProperty("user.dir") + "/";
+
+
+    public static <T> void writeToFile(String path, List<T> data, boolean isAppendMode) {
+
+        Path filePath = Path.of(path);
+        try {
+            if (Files.notExists(filePath)) {
+                Files.createFile(filePath);
+            }
+
+            try (PrintWriter writer = new PrintWriter(new FileWriter(filePath.toFile(), isAppendMode))) {
+                for (T item : data) {
+                    writer.println(item);
+                }
+                System.out.println("Data successfully written to the file: " + path);
+            }
+            } catch (IOException err) {
+                System.err.println("Error: could not write to file: " + path + " " + err.getMessage());
+            }
     }
 }
