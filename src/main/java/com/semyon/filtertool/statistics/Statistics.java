6fb6f24be0215629e233ef1424c007718d4f72e3
Semyon <s.borodin@g.nsu.ru>
2025-02-18 19:43:48 +0700

    statistics updated for tests

--- src/main/java/com/semyon/filtertool/statistics/Statistics.java
+++ src/main/java/com/semyon/filtertool/statistics/Statistics.java
@@ -1,4 +1,18 @@
 package com.semyon.filtertool.statistics;
 
 public class Statistics {
+
+    public static String getShortStatistics(){
+        //Должна посчитать чисто успешно записаннных файлов
+        System.out.println("Заглушка -- тест короткой статистики");
+        return "test smt";
+    }
+    public static String getFullStatistics(){
+        //Полная статистика для чисел
+        //дополнительно содержит минимальное и максимальное значения, сумма и среднее.
+        //Полная статистика для строк, помимо их количества, содержит также размер самой
+        //короткой строки и самой длинной.
+        System.out.println("Заглушка -- тест полной статистики");
+        return "test smt";
+    }
 }
